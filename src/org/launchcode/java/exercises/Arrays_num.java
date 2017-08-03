a51dd0a2d7b4780b7086875bdf8945fb13675c77
Niels Dh <nielsdhollander@gmail.com>
2017-08-03 12:28:36 -0700

    Added third exercise of class 3.2 prep

---
+++ src/org/launchcode/java/exercises/Arrays_num.java
@@ -0,0 +1,11 @@
+package org.launchcode.java.exercises;
+
+public class Arrays_num {
+    public static void main(String[] args){
+        int[] numbers = {1,1,2,3,5,8};
+        for(int num:numbers){
+            System.out.println(num);
+        }
+
+    }
+}
